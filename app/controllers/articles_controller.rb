2a2a11819323b0e8a35c4dfbc664512b17477cd1
keroropper <somato.0311@gmail.com>
2021-02-04 16:44:51 +0900

    ニュースAPI導入　株のニュースを表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
+  require 'open-uri'
+  
+  before_action :stock_news
+
   def index
   end
+
+
+  private
+
+  def stock_news
+    api = Rails.application.credentials.news_api[:api_key]
+    uri = "https://newsapi.org/v2/everything?q=%E6%A0%AA%E4%BE%A1&sortBy=popularity&apiKey=#{api}"
+    req = open(uri)
+    article_serialized = open(uri).read
+    @article_news = JSON.parse(article_serialized) 
+  end
+
 end
